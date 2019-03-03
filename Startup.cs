f8296f5052df3be477a2490bd897f504cf48644a
dgp <dgp-web@outlook.de>
2019-03-03 18:47:45 +0100

    Extract Program.cs and cleanup Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -10,42 +10,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 
-
 namespace app
 {
-
-     public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
-    }
 
     public class Startup
     {
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
-            services.AddNodeServices();
-            services.AddMvc();
+      	    services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseCors(builder => builder
-                 .AllowAnyHeader()
-                 .AllowAnyMethod()
-                 .AllowAnyOrigin()
-             );
 
             loggerFactory.AddConsole();
 
