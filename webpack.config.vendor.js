a85e226dfdca3ea1dfcdd40ee93f177a4ef55a9b
David Brem <david.brem@evorpo-ag.de>
2019-03-04 15:36:04 +0100

    Mute fesm5 warnings in webpack.config.vendor

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -4,39 +4,44 @@ const merge = require('webpack-merge');
 
 module.exports = {
 
-        stats: { modules: false },
-        mode: 'development',
-        resolve: {
-            extensions: ['.js']
-        },
-        entry: {
-            vendor: [
-                'core-js/shim',
-                'zone.js/dist/zone',
-                'event-source-polyfill',
-                'rxjs',
-                '@angular/common',
-                '@angular/compiler',
-                '@angular/core',
-                '@angular/forms',
-                '@angular/platform-browser',
-                '@angular/platform-browser-dynamic',
-                '@angular/router',
-            ]
-        },
-        output: {
-            filename: '[name].js',
-            library: '[name]_[hash]',
-            publicPath: '/',
-            path: path.join(__dirname, 'wwwroot')
-        },
-        plugins: [
-            new webpack.ContextReplacementPlugin(/\@angular\b.*\b(bundles|linker)/, path.join(__dirname, './Client')),
-            new webpack.IgnorePlugin(/^vertx$/),
-            new webpack.DllPlugin({
-                path: path.join(__dirname, 'wwwroot', '[name]-manifest.json'),
-                name: '[name]_[hash]'
-            })
-        ]
+  stats: {modules: false},
+  mode: 'development',
+  resolve: {
+    extensions: ['.js']
+  },
+  entry: {
+    vendor: [
+      'core-js/shim',
+      'zone.js/dist/zone',
+      'event-source-polyfill',
+      'rxjs',
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/forms',
+      '@angular/platform-browser',
+      '@angular/platform-browser-dynamic',
+      '@angular/router',
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    library: '[name]_[hash]',
+    publicPath: '/',
+    path: path.join(__dirname, 'wwwroot')
+  },
+  module: {
+    rules: [{
+      test: /[\/\\]@angular[\/\\]core[\/\\].+\.js$/,
+      parser: {system: true},
+    }]
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(/(.+)?angular(\\|\/)core(.+)?/, path.join(__dirname, 'src'), {}),
+    new webpack.DllPlugin({
+      path: path.join(__dirname, 'wwwroot', '[name]-manifest.json'),
+      name: '[name]_[hash]'
+    })
+  ]
 
 };
