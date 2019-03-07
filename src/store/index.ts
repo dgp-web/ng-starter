65b08f981ed43abcfb5d9d7fad5fe1cd8c58846c
dgp <dgp-web@outlook.de>
2019-03-07 06:32:13 +0100

    Add store module

---
+++ src/store/index.ts
@@ -0,0 +1,6 @@
+/**
+ * The store is the main source of data for
+ * our application
+ */
+export * from "./actions";
+export * from "./store.module";
---
+++ src/store/store.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+
+@NgModule({
+    imports: [
+    ],
+    declarations: [],
+    exports: [],
+    providers: []
+})
+export class AppStoreModule {
+}
